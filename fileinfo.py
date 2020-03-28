3f600987fe93a8d21018df00f5715f2a68c0380e
nathniel <1040490912@qq.com>
2020-03-28 21:10:48 +0800

    initial update

---
+++ fileinfo.py
@@ -0,0 +1,8 @@
+# script name :fileinfo.py
+# Author : nathaniel cheng
+# created: 2019.1
+# Last modified :
+# version :1.0
+# modifications :
+
+#description :show file information for a given file
